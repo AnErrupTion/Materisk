91d633f501859010aa9e593484ea2b4146b34fd6
AnErrupTion <anerruption@disroot.org>
2023-03-24 21:10:49 +0100

    Implement binary expression node

--- Materisk/Parsing/Nodes/BinaryExpressionNode.cs
+++ Materisk/Parsing/Nodes/BinaryExpressionNode.cs
@@ -1,5 +1,6 @@
 ﻿using AsmResolver.DotNet;
 using AsmResolver.DotNet.Code.Cil;
+using AsmResolver.PE.DotNet.Cil;
 using Materisk.BuiltinTypes;
 
 namespace Materisk.Parsing.Nodes;
@@ -21,29 +22,46 @@ internal class BinaryExpressionNode : SyntaxNode
 
     public override SValue Evaluate(Scope scope)
     {
-        var leftRes = left.Evaluate(scope);
-        var rightRes = right.Evaluate(scope);
-
-        return operatorToken.Type switch
-        {
-            SyntaxType.Plus => leftRes.Add(rightRes),
-            SyntaxType.Minus => leftRes.Sub(rightRes),
-            SyntaxType.Div => leftRes.Div(rightRes),
-            SyntaxType.Mul => leftRes.Mul(rightRes),
-            SyntaxType.Mod => leftRes.Mod(rightRes),
-            SyntaxType.EqualsEquals => leftRes.Equals(rightRes),
-            SyntaxType.Idx => leftRes.Idx(rightRes),
-            SyntaxType.LessThan => leftRes.LessThan(rightRes),
-            SyntaxType.LessThanEqu => leftRes.LessThanEqu(rightRes),
-            SyntaxType.GreaterThan => leftRes.GreaterThan(rightRes),
-            SyntaxType.GreaterThanEqu => leftRes.GreaterThanEqu(rightRes),
-            _ => throw new NotImplementedException()
-        };
+        return null;
     }
 
     public override object Emit(Dictionary<string, CilLocalVariable> variables, ModuleDefinition module, MethodDefinition method, List<string> arguments)
     {
-        throw new NotImplementedException();
+        left.Emit(variables, module, method, arguments);
+        right.Emit(variables, module, method, arguments);
+
+        switch (operatorToken.Type)
+        {
+            case SyntaxType.Plus: method.CilMethodBody?.Instructions.Add(CilOpCodes.Add); break;
+            case SyntaxType.Minus: method.CilMethodBody?.Instructions.Add(CilOpCodes.Sub); break;
+            case SyntaxType.Div: method.CilMethodBody?.Instructions.Add(CilOpCodes.Div); break;
+            case SyntaxType.Mul: method.CilMethodBody?.Instructions.Add(CilOpCodes.Mul); break;
+            case SyntaxType.Mod: method.CilMethodBody?.Instructions.Add(CilOpCodes.Rem); break;
+            case SyntaxType.EqualsEquals: method.CilMethodBody?.Instructions.Add(CilOpCodes.Ceq); break;
+            case SyntaxType.Idx:
+            {
+                throw new NotImplementedException();
+            }
+            case SyntaxType.LessThan: method.CilMethodBody?.Instructions.Add(CilOpCodes.Clt); break;
+            case SyntaxType.LessThanEqu:
+            {
+                method.CilMethodBody?.Instructions.Add(CilOpCodes.Cgt);
+                method.CilMethodBody?.Instructions.Add(CilOpCodes.Ldc_I4_0);
+                method.CilMethodBody?.Instructions.Add(CilOpCodes.Ceq);
+                break;
+            }
+            case SyntaxType.GreaterThan: method.CilMethodBody?.Instructions.Add(CilOpCodes.Cgt); break;
+            case SyntaxType.GreaterThanEqu:
+            {
+                method.CilMethodBody?.Instructions.Add(CilOpCodes.Clt);
+                method.CilMethodBody?.Instructions.Add(CilOpCodes.Ldc_I4_0);
+                method.CilMethodBody?.Instructions.Add(CilOpCodes.Ceq);
+                break;
+            }
+            default: throw new NotImplementedException();
+        }
+
+        return null;
     }
 
     public override IEnumerable<SyntaxNode> GetChildren()
