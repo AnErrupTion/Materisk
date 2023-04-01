df11a83b7e22af9152821beaf2d8fc4cddee05d6
AnErrupTion <anerruption@disroot.org>
2023-04-01 19:40:05 +0200

    Add missing file

---
+++ MateriskLLVM/LlvmUtils.cs
@@ -0,0 +1,10 @@
+using LLVMSharp.Interop;
+
+namespace MateriskLLVM;
+
+public static class LlvmUtils
+{
+    public static readonly LLVMValueRef ByteZero = LLVMValueRef.CreateConstInt(LLVMTypeRef.Int8, 0, true);
+    public static readonly LLVMValueRef IntZero = LLVMValueRef.CreateConstInt(LLVMTypeRef.Int32, 0, true);
+    public static readonly LLVMValueRef FloatZero = LLVMValueRef.CreateConstInt(LLVMTypeRef.Float, 0, true);
+}
